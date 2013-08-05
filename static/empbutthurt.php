b63393063e23dae854a6075e68ca0a936bf54efe
thetammy0r <tamara.wisdom@wiztech.biz>
2013-08-04 20:58:53 -0600

    Create empbutthurt.php

---
+++ static/empbutthurt.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Empornium Butthurt report form</title>
+            <style type="text/css">
+            html {
+                font-size: 100%;
+                -webkit-text-size-adjust: 100%;
+                -ms-text-size-adjust: 100%;
+            }
+            a:focus {
+                outline: thin dotted #333;
+                outline: 5px auto -webkit-focus-ring-color;
+                outline-offset: -2px;
+            }
+
+            a:hover,
+            a:active {
+                outline: 0;
+            }
+            body {
+                margin: 0;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                font-size: 14px;
+                line-height: 20px;
+                color: #333333;
+                background-color:#ff69b4;
+                padding:20px 72px;
+            }
+
+            a {
+                color: #0088cc;
+                text-decoration: none;
+            }
+
+            a:hover,
+            a:focus {
+                color: #005580;
+                text-decoration: underline;
+            }
+h1,
+            h2,
+            h3,
+            h4,
+            h5,
+            h6 {
+                margin: 10px 0;
+                font-family: inherit;
+                font-weight: bold;
+                line-height: 20px;
+                color: inherit;
+                text-rendering: optimizelegibility;
+            }
+            h1,
+            h2,
+            h3 {
+                line-height: 40px;
+            }
+
+            h1 {
+                font-size: 38.5px;
+            }
+
+            h2 {
+                font-size: 31.5px;
+            }
+
+            h3 {
+                font-size: 24.5px;
+            }
+
+            p {
+                margin: 0 0 10px;
+            }
+
+            .lead {
+                margin-bottom: 20px;
+                font-size: 21px;
+                font-weight: 200;
+                line-height: 30px;
+            }
+            address {
+            display: block;
+                margin-bottom: 20px;
+                font-style: normal;
+                line-height: 20px;
+            }
+        </style>
+</head>
+
+<body>
+<h1>Empornium Butthurt Form</h1>
+<?php $currentDate = date("D d M Y \@ G:i:s \U\T\C"); ?>
+<p> </p>
+<p>Date of butthurt: <?php echo $currentDate ; ?></p>
+<p> </p>
+<h2>Please describe the location of the butthurt (tick as appropriate)</h2>
+<p>⌑ Forum Thread</p>
+<p>⌑ Private Message</p>
+<p>⌑ Picture from a torrent</p>
+<p>⌑ Video from a torrent</p>
+<p>⌑ Hexensabbat related incident (please attach additional pages of documentation)</p>
+<p>⌑ Bitman64 related incident (please attach additional pages of documentation)</p>
+<p>⌑ CasuallyDressed related incident (please attach additional pages of documentation)</p>
+<p>⌑ Other (please specify)</p>
+<p> </p>
+<h2>Describe the nature of the butthurt</h2>
+<p>⌑ Someone made a post I did not agree with</p>
+<p>⌑ Someone wrote about a fantasy or had a fetish that offended me</p>
+<p>⌑ Someone forgot to use my title or correct capitalization (IE: Sir Master LordStabby McKnifey Pants)</p>
+<p>⌑ Someone posted pornography that I cannot unsee</p>
+<p>⌑ Someone told me they would like to unsee the pornography I posted</p>
+<p>⌑ I lost an argument in a thread</p>
+<p>⌑ I attacked a moderator/admin and lost and feel violatedi</p>
+<p>⌑ I am a misogynist and am horrified that this site has female staff members, How dare you!</p>
+<p>⌑ Lefties and right wing facists or those into religion or against iti</p>
+<p>⌑ I am offended that you index gay/lesbian/transgender pornography as it goes against my religious/personal morals (yet your on a porn site??!?) </p>
+p>⌑ Hexensabbat related incident (please attach additional pages of documentation)</p>
+<p>⌑ Bitman64 related incident (please attach additional pages of documentation)</p>
+<p>⌑ CasuallyDressed related incident (please attach additional pages of documentation)</p>
+<p>⌑ Other (Please specify on a separate sheet and attach)</p>
+<p> </p>
+<p> </p>
+<br><br>
+<p>(Y) (N) Did you cry about the Butthurt?</p>
+<p>(Y) (N) Did the Butthurt cause you irrevocable emotional damage?</p>
+<p>(Y) (N) Was sleep lost over the Butthurt?</p>
+<p>(Y) (N) Did Butthurt interrupt your daily schedule (IE: were you late to adate, appointment or work?)</p>
+<p>(Y) (N) Did you tell your friends about the Butthurt?</p>
+<p>(Y) (N) Do your hands ache from typing long-ass Butthurt rebuttals?</p>
+<p>(Y) (N) Were you forced to do something in meatspace to quell your  anger(IE: Did you take deep breaths or walk away from the computer?)</p>
+<p> </p>
+<h2>Please describe the person who caused the Butthurt</h2>
+<p>⌑ They are not a True Dominant / submissive.</p>
+<p>⌑ They are the ones who give the Empornium Community a Bad Name.</p>
+<p>⌑ Unlike me they did not learn the rules of Empornium.</p>
+<p>⌑ People like that are sick and deserve to be rounded up and killed.</p>
+<p>⌑ They are not old enough to know the real meaning of BDSM.</p>
+<p>⌑ They are stunted in their Spiritual Journey.</p>
+<p>⌑ Their gender is inherently unequal and should be subjugated.</p>
+<p>⌑ An admin / senior technologist / moderator named ___________________</p>
+<p>⌑ Other (Please specify on a separate sheet and attach)</p>
+<h2>What did you do about the Butthurt?</h2>
+<p>⌑ I wrote an incoherent essay detailing the extent of my Butthurt.</p>
+<p>⌑ I got my Family to join empornium so together we can flame the cause of my butthurt.</p>
+<p>⌑ I attempted to trick staff into banning their asses from the site.</p>
+<p>⌑ I posted their RL information and/or outed them.</p>
+<p>⌑ I created a dummy account to escape the shame and/or flame them.</p>
+<p>⌑ I joined another porn site</p>
+<p>⌑ I realized that arguing on Empornium is pointless and did something else with mytime.</p>
+<p> </p>
+<p> </p>
+<h2>When complete please snail mail or fax this to the following:</h2>
+<p>Empornium
+<br>C/O Westboro Baptist Church
+<br>ATTN: Pastor Fred Phelps
+<br>3701 SW 12th S
+<br>Topeka, KS 66604
+<br>+1 (785) 273-0325 (main) (feel free to call collect if the butthurt is severe! Admins are standing by now to take your call. feel free to call 24/7/365 as we strive for optimal customer service!)
+<br>+1 (900) FUCK-YOU (fax)</p>
+<h3>Or use our west coast address:</h3>
+<p>Empornium Ltd
+<br>Butthurt derp.
+<br>15301 Ventura Blvd., Building E
+<br>Sherman Oaks, CA 91403
+<br>+1 (818) 995-6600 (main)
+<br>+1 (818) 285-4403 (fax)</p>
+</body>
+</html>
