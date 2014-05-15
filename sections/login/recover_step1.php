61d5b5a42ae4c905013a945e9e8a9edc6ed4881e
LordRandom <mrr@digitalirc.org>
2014-05-15 21:07:35 +0100

    Update webchat URL & add https

--- sections/login/recover_step1.php
+++ sections/login/recover_step1.php
@@ -55,7 +55,7 @@ if(empty($_POST['submit']) || empty($_POST['username'])) {
             <div class="">
                   <div class="head">IRC</div>
                   <div class="box pad center"> 
-                            <iframe src="http://shire.digitalirc.org:9090/?prompt=1&uio=MTY9ZmFsc2U67&nick=<?=$nick?>&channels=empornium-help" width="98%" height="600"></iframe> 
+                            <iframe src="https://webchat.digitalirc.org/?prompt=1&uio=MTY9ZmFsc2U67&nick=<?=$nick?>&channels=empornium-help" width="98%" height="600"></iframe> 
                   </div>
             </div>
         </div>
